88fef988026bb300146330af77d76859f7cc60de
svnflxwer <azgill3@gmail.com>
2024-02-07 16:23:53 +0700

    coba py baru 2

--- ora.py
+++ ora.py
@@ -13,7 +13,7 @@ def check_cron_jobs_status_ora():
     db_params_ora  = {
         'user'      : 'system',
         'password'  : 'sinatriaba',
-        'dsn'       : 'localhost:1521/XE'
+        'dsn'       : 'dsa:1521/XE'
     }
 
     # Create a list to store the names of offline jobs
