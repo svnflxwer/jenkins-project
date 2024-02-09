c985f0337e9b741c249b0d91985f92c4e3a629c2
svnflxwer <azgill3@gmail.com>
2024-02-09 16:01:48 +0700

    First Push

--- insertPG.py
+++ insertPG.py
@@ -1,15 +1,14 @@
 import psycopg2
-import cx_Oracle
 import json
 
 def check_cron_jobs_status_pg():
     # Database connection parameters
     db_params_pg  = {
-        'database': 'dummydb',
-        'user': 'postgres',
-        'password': 'sinatriaba',
-        'host': 'localhost',
-        'port': '5432'
+        'database'  : 'dummydb',
+        'user'      : 'postgres',
+        'password'  : 'sinatriaba',
+        'host'      : 'localhost',
+        'port'      : '5432'
     }
 
     # Create a list to store the names of offline jobs
@@ -17,11 +16,19 @@ def check_cron_jobs_status_pg():
 
     try:
         # Connect to the database
+        print("MASUK KE POSTGRE...")
         connection_pg   = psycopg2.connect(**db_params_pg)
         cursor_pg       = connection_pg .cursor()
 
         # Query 
-        query_pg        = "SELECT kode_karyawan, nama, jabatan FROM karyawan"
+        query_pg        = """
+                            SELECT
+                                kode_karyawan,
+                                nama,
+                                jabatan
+                            FROM
+                                karyawan
+                        """
         cursor_pg.execute(query_pg )
         
         # Fetch all records from PostgreSQL
@@ -44,52 +51,9 @@ def check_cron_jobs_status_pg():
             connection_pg.close()
 
 
-# def check_cron_jobs_status_ora():
-#     # Database connection parameters
-#     db_params_ora  = {
-#         'user'      : 'system',
-#         'password'  : 'sinatriaba',
-#         'dsn'       : 'localhost:1521/XE'
-#     }
-
-#     # Create a list to store the names of offline jobs
-#     karyawan_ora  = []
-
-#     try:
-#         # Connect to the database
-#         connection_ora  = cx_Oracle.connect(**db_params_ora)
-#         cursor_ora      = connection_ora.cursor()
-
-#         # Query 
-#         query_ora  = "SELECT KODE_KARYAWAN, NAMA, JABATAN FROM KARYAWAN"
-#         cursor_ora.execute(query_ora)
-
-#         # Fetch all records from Oracle
-#         records_ora = cursor_ora.fetchall()
-
-#         for record in records_ora:
-#             kode_pegawai, nama, jabatan = record
-#             if kode_pegawai:
-#                 karyawan_ora.append(kode_pegawai)
-
-#         # Return the list of offline jobs as a JSON string
-#         return json.dumps(karyawan_ora)
-
-#     except Exception as e:
-#         return json.dumps({"error_ora": str(e)})
-#     finally:
-#         if 'cursor_ora' in locals():
-#             cursor_ora.close()
-#         if 'connection_ora' in locals():
-#             connection_ora.close()
-
 if __name__ == "__main__":
     karyawan_pg     = check_cron_jobs_status_pg()
-    # karyawan_ora    = check_cron_jobs_status_ora()
     
     # Print the results
     print("PostgreSQL Result:")
     print(karyawan_pg)
-
-    # print("Oracle Result:")
-    # print(karyawan_ora)
--- selectOra.py
+++ selectOra.py
@@ -2,14 +2,7 @@ import cx_Oracle
 import json
 import os, platform
 def check_cron_jobs_status_ora():
-    # lib_dir = r"/home/sinatriaba/instantclient_11_2"
-    # print("ARCH:", platform.architecture())
-    # print("FILES AT lib_dir:")
-    # for root, dirs, files in os.walk(lib_dir):
-    #     for name in files:
-    #         print(os.path.join(root, name))
-    # cx_Oracle.init_oracle_client(config_dir="/mnt/d/MAGANG-SINAT/oracle-database-xe-11g/app/oracle/product/11.2.0/server/network/ADMIN")
-    # Database connection parameters
+
     db_params_ora  = {
         'user'      : 'jenkinsdb',
         'password'  : 'sinatriaba',
@@ -21,12 +14,19 @@ def check_cron_jobs_status_ora():
 
     try:
         # Connect to the database
-        print("MASUK  KE ORACLE...")
+        print("MASUK KE ORACLE...")
         connection_ora  = cx_Oracle.connect(**db_params_ora)
         print(connection_ora)
         cursor_ora      = connection_ora.cursor()
         # Query 
-        query_ora  = "SELECT KODE_PEGAWAI, NAMA, JABATAN FROM KARYAWAN_IT"
+        query_ora  = """
+                        SELECT 
+                            KODE_PEGAWAI, 
+                            NAMA, 
+                            JABATAN 
+                        FROM 
+                            KARYAWAN_IT
+                    """
         cursor_ora.execute(query_ora)
 
         # Fetch all records from Oracle
