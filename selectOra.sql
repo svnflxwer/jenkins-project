6be2ad333baf574da2f58af0bb92b75320cdcd7c
giovannih <71913818+giovannih@users.noreply.github.com>
2024-03-11 14:44:28 +0700

    change col name

--- selectOra.sql
+++ selectOra.sql
@@ -1,10 +1,10 @@
 SET LINESIZE 1000 TRIMSPOOL ON PAGESIZE 0 FEEDBACK OFF
 SPOOL /var/lib/jenkins/dataCsvTemp/finance-dept_transaksi-penjualan-retail_ora-to-pg.csv
 select 
-    'ID_TRANSAKSI|ID_FRANCHAISE|FRANCHAISE|TANGGAL_TRANSAKSI|ID_PRODUK|NAMA_PRODUK|JUMLAH_TERJUAL|STOCK|DISCOUNT|PPN|PPH4|PPH23|STATUS_PEMBAYARAN|TANGGAL_PEMBAYARAN' 
+    'ID_TRANSAKSI|ID_FRANCHISE|FRANCHISE|TANGGAL_TRANSAKSI|ID_PRODUK|NAMA_PRODUK|JUMLAH_TERJUAL|STOCK|DISCOUNT|PPN|PPH4|PPH23|STATUS_PEMBAYARAN|TANGGAL_PEMBAYARAN' 
 from dual;
 select 
-    id_transaksi || '|' || id_franchaise || '|' || franchaise || '|' || tanggal_transaksi || '|' || id_produk || '|' || nama_produk || '|' || jumlah_terjual || '|' || stock || '|' || discount || '|' || PPN || '|' || PPH4 || '|' || PPH23 || '|' || status_pembayaran || '|' || tanggal_pembayaran 
+    id_transaksi || '|' || id_FRANCHISE || '|' || FRANCHISE || '|' || tanggal_transaksi || '|' || id_produk || '|' || nama_produk || '|' || jumlah_terjual || '|' || stock || '|' || discount || '|' || PPN || '|' || PPH4 || '|' || PPH23 || '|' || status_pembayaran || '|' || tanggal_pembayaran 
 from
     transaksi_penjualan_retail
 order by id_transaksi;
